138a54b5159b5fcd55b554d4f653ab70c5c4b6b6
Ismail Alsmail <121337727+IsmailTech365@users.noreply.github.com>
2024-03-07 05:16:40 +0300

    Add files via upload

---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:facebook_clone/data/data.dart';
+import 'package:facebook_clone/screens/screens.dart';
+import 'package:facebook_clone/widgets/widgets.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({super.key});
+
+  @override
+  _NavScreenState createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const Scaffold(),
+    const Scaffold(),
+    const Scaffold(),
+    const Scaffold(),
+    const Scaffold(),
+  ];
+  final List<IconData> _icons = const [
+    Icons.home,
+    Icons.ondemand_video,
+    Icons.account_circle,
+    Icons.account_balance_outlined,
+    Icons.bed_outlined,
+    Icons.menu,
+  ];
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return DefaultTabController(
+      length: _icons.length,
+      child: Scaffold(
+        appBar: Responsive.isDesktop(context)
+            ? PreferredSize(
+                preferredSize: Size(screenSize.width, 100.0),
+                child: CustomAppBar(
+                  currentUser: currentUser,
+                  icons: _icons,
+                  selectedIndex: _selectedIndex,
+                  onTap: (index) => setState(() => _selectedIndex = index),
+                ),
+              )
+            : null,
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _screens,
+        ),
+        bottomNavigationBar: !Responsive.isDesktop(context)
+            ? Container(
+                padding: const EdgeInsets.only(bottom: 12.0),
+                color: Colors.white,
+                child: CustomTabBar(
+                  icons: _icons,
+                  selectedIndex: _selectedIndex,
+                  onTap: (index) => setState(() => _selectedIndex = index),
+                ),
+              )
+            : const SizedBox.shrink(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/more_options_list.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:facebook_clone/config/palette.dart';
+import 'package:facebook_clone/models/models.dart';
+import 'package:facebook_clone/widgets/widgets.dart';
+
+class MoreOptionsList extends StatelessWidget {
+  final List<List> _moreOptionsList = const [
+    [Icons.account_balance, Colors.deepPurple, 'COVID-19 Info Center'],
+    [Icons.account_balance_outlined, Colors.cyan, 'Friends'],
+    [Icons.messenger, Palette.facebookBlue, 'Messenger'],
+    [Icons.flag, Colors.orange, 'Pages'],
+    [Icons.storefront, Palette.facebookBlue, 'Marketplace'],
+    [Icons.ondemand_video, Palette.facebookBlue, 'Watch'],
+    [Icons.calendar_month, Colors.red, 'Events'],
+  ];
+
+  final User currentUser;
+
+  const MoreOptionsList({
+    Key? key,
+    required this.currentUser,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 280.0),
+      child: ListView.builder(
+        itemCount: 1 + _moreOptionsList.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: UserCard(user: currentUser),
+            );
+          }
+          final List option = _moreOptionsList[index - 1];
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: _Option(
+              icon: option[0],
+              color: option[1],
+              label: option[2],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _Option extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String label;
+
+  const _Option({
+    Key? key,
+    required this.icon,
+    required this.color,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => print(label),
+      child: Row(
+        children: [
+          Icon(icon, size: 38.0, color: color),
+          const SizedBox(width: 6.0),
+          Flexible(
+            child: Text(
+              label,
+              style: const TextStyle(fontSize: 16.0),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
